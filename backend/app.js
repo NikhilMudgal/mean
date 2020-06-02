1824aab1604f8c14002177fa1e8e4e8049e3a236
Nikhil-Mudgal-Learnings <nikhil.mudgal@outlook.com>
2020-06-03 01:32:56 +0530

    Setup basic Backend

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,47 @@
-const express = require('express');
+const express = require("express");
 
-const app = express();  // this will return an express app
+const app = express(); // this will return an express app
 
-app.use((req, res, next) =>{      // use uses the middleware on our app and on the incoming request
-  console.log('First MiddleWare');
-  next();  // if next function is executed, then the request will actually continue its journey
+// app.use((req, res, next) =>{      // use uses the middleware on our app and on the incoming request
+//   console.log('First MiddleWare');
+//   next();  // if next function is executed, then the request will actually continue its journey
 
-});   // use middleware on our app and on the incoming request
+// });   // use middleware on our app and on the incoming request
 
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
+  // First Argument is Header Key and second argument is value for that header
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  // line 13 means no matter which domain the app which is sending the request is running on, it is allowed to access our resources
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  // First line allows which domain can access our resources and the second statement tells the incoming request may have these headers
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  ); // it tells which http verbs may be used to send requests
+  next();
+});
 
-      // without it will not continue travel down other middle wares and
-  res.send('Hello from express');
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      Id: "154651cds535",
+      title: "This is a post",
+      content: "This is coming from server",
+    },
+    {
+      Id: "789fgfd8g7",
+      title: "This is a second post",
+      content: "This is coming from server as well",
+    },
+  ];
+  // without it will not continue travel down other middle wares and
+  return res.status(200).json({
+    posts: posts,
+    message: "These are coming from app.js file",
+  });
 });
 
 module.exports = app;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatExpansionModule } from '@angular/material/expansion'
+import { MatExpansionModule } from '@angular/material/expansion';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
-import { PostService } from './posts/posts.service';
 
 @NgModule({
   declarations: [
@@ -30,9 +30,10 @@ import { PostService } from './posts/posts.service';
     MatButtonModule,
     MatCardModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
-  providers: [PostService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
