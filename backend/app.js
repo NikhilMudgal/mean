e8170c4db57ceec3834ebab5e467798e214859b3
Nikhil-Mudgal-Learnings <nikhil.mudgal@outlook.com>
2020-06-09 02:56:09 +0530

    Reorganised Backend Routes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const Post = require("./models/post");
+
+const postRoutes = require("./routes/posts");
 
 const app = express(); // this will return an express app
 
@@ -47,58 +48,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  console.log(post);
-  post.save()   // save() is provided by mongoose package itself
-  .then(createdPost => {
-    res.status(201).json({
-      message: "Post Added Successfully",
-      postId: createdPost._id
-    });
-  });
-  // the data will be saved in the collection whose name will be the plural name of the model and will be created automatically
-
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find()
-  .then(documents => {
-    console.log(documents);
-    return res.status(200).json({
-      posts: documents,
-      message: "These are coming from app.js file",
-    });
-  });    // find() simply return all entries
-  // without next() it will not continue travel down other middle wares and
-
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if(post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({message: "Post not found"});
-    }
-  })
-});
-
-app.put("/api/posts/:id", (req,res,next) => {
-  // put() will replace the existing object while patch updates the existing object
-  const post = new Post({ _id: req.body.Id, title: req.body.title, content: req.body.content})
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    res.status(200).json({message: "Update Successfully"});
-  })
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({_id: req.params.id}).then(result => {
-    res.status(200).json({message: "Post Deleted"});
-  });
-});
+app.use("/api/posts", postRoutes); // routes declared for posts in posts.js file of routes folder are now known by app.js
 
 module.exports = app;
