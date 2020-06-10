9212fca7056cf77eb4d6fda666ff925cc0b30491
Nikhil-Mudgal-Learnings <nikhil.mudgal@outlook.com>
2020-06-10 09:30:18 +0530

    Created Image Validator

---
+++ src/app/posts/post-create/mime-type.validator.ts
@@ -0,0 +1,47 @@
+import { AbstractControl } from '@angular/forms';
+import { Observable, Observer } from 'rxjs';
+
+/* This will be asynchronous Validator as reading a file takes time.
+The Output for synchronous validator is object which return error if invalid and
+null if valid. The output for asynchronous validator will be similar along with Promise and Observable.
+The Property key will be dynamic and we dont care about the property key name.
+So it is declared as dynamic by writing it in the square bracket.
+*/
+export const mimeType =  (control: AbstractControl): Promise<{[key: string]: any}> | Observable<{[key: string]: any}> => {
+
+  const file = control.value as File;
+  const fileReader = new FileReader();
+  const frObs = new Observable((observer: Observer<{[key: string]: any}>) => {
+    fileReader.addEventListener('loadend', () => {
+      const arr = new Uint8Array(fileReader.result as ArrayBuffer).subarray(0, 4);
+      let header = '';
+      let isValid = false;
+      for (let i = 0 ; i < arr.length; i++) {
+        header += arr[i].toString(16);
+      }
+      switch (header) {
+        case '89504e47':
+          isValid = true;
+          break;
+        case 'ffd8ffe0':
+        case 'ffd8ffe1':
+        case 'ffd8ffe2':
+        case 'ffd8ffe3':
+        case 'ffd8ffe8':
+          isValid = true;
+          break;
+        default:
+          isValid = false; // Or you can use the blob.type as fallback
+          break;
+      }
+      if (isValid) {
+        observer.next(null);
+      } else {
+        observer.next({ invalidMimeType: true });
+      }
+      observer.complete();
+    });
+    fileReader.readAsArrayBuffer(file);
+  });
+  return frObs;
+};
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,7 +3,7 @@ import { Post } from '../posts.model';
 import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-
+import { mimeType } from './mime-type.validator';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
@@ -29,7 +29,7 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required, Validators.minLength(3)],
       }),
       content: new FormControl(null, { validators: [Validators.required] }),
-      image: new FormControl(null, {validators: [Validators.required]})
+      image: new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
