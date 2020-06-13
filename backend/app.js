51c40618430718cbcc648652a213aadb82b6d6b4
Nikhil-Mudgal-Learnings <nikhil.mudgal@outlook.com>
2020-06-14 01:26:45 +0530

    Displayed Image in the front end

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -31,6 +32,7 @@ mongoose
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req, res, next) => {
   // First Argument is Header Key and second argument is value for that header
