c44788b0121edc52b5c23fdae52aa5bc68a7a589
Nikhil Mudgal <nikhilm2097@gmail.com>
2023-03-19 01:02:51 +0530

    Created User Login Api in backend, created auth token and passing in front end

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bcrypt= require('bcrypt');
+const jwt= require('jsonwebtoken');
 
 const User = require("../models/user")
 
@@ -27,6 +28,41 @@ router.post("/signup", (req,res,next) => {
       })
     })
     
+})
+
+router.post("/login", (req,res,next) => {
+    // to login we are first validating the email
+    console.log(req.body.email)
+    let fetchedUser;
+    User.findOne({ email: req.body.email})
+    .then((user) => {
+        console.log('user', user)
+        if(!user) {
+            return res.status(401).json({
+                message: 'Auth Failed'
+            })
+        }
+        fetchedUser = user
+        return bcrypt.compare(req.body.password, user.password) // we have encrypted the password but cannot decrypt it. So we are comparing the input value(unencrypted) with the encrypted value using bcrypt.compare()
+    })
+    .then(result => {
+        console.log(result);
+        if(!result) {
+            return res.status(401).json({
+                message: 'Auth Failed'
+            })
+        }
+        const token = jwt.sign({email: fetchedUser.email, userId: fetchedUser._id}, 'secret_this_should_be_longer', {expiresIn: '1h'});
+        console.log(token)
+        res.status(200).json({
+            token
+        })
+    }).catch(err => {
+        console.log(err);
+        return res.status(401).json({
+            message: 'Auth Failed'
+        })
+    })
 })
 
 module.exports = router
