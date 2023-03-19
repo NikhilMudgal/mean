c015e81dba55d93fcab1af7fa45952875a880dd8
Nikhil Mudgal <nikhilm2097@gmail.com>
2023-03-19 22:52:52 +0530

    Added checkAuth middleware in post routes

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -4,8 +4,9 @@ module.exports = (req, res, next) => {
     try {
 
       const token = req.headers.authorization.split(" ")[1];
-      // to verify the token jwt uses verify() function along with token and secret key used to create that token
+      // to verify the token jwt uses verify() function along with token and secret key used to create that token and will throw an error if token is invalid
       jwt.verify(token, "secret_this_should_be_longer")
+      next()
     } catch(error) {
         res.status(401).json({message: "Auth Failed"})
     }
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,6 +3,7 @@ const multer = require('multer');  // Multer is used to extract incoming files
 const router = express.Router();
 
 const Post = require("../models/post");
+const checkAuth = require("../middleware/check-auth")
 
 const MIME_TYPE_MAP =  {
   'image/png': 'png',
@@ -29,7 +30,7 @@ const storage = multer.diskStorage({
 });
 
 
-router.post("", multer({storage: storage}).single("image"), (req, res, next) => {
+router.post("", checkAuth,multer({storage: storage}).single("image"), (req, res, next) => {
   // multer gives info about the file to be stored in the req body
   const url = req.protocol + '://' + req.get("host")  // this constructs url to our server
   const post = new Post({
@@ -87,7 +88,7 @@ router.get("/:id", (req, res, next) => {
   })
 });
 
-router.put("/:id", multer({storage: storage}).single("image"), (req,res,next) => {
+router.put("/:id", checkAuth,multer({storage: storage}).single("image"), (req,res,next) => {
   // put() will replace the existing object while patch updates the existing object
   let imagePath = req.body.imagePath;
   if(req.file) {
@@ -101,7 +102,7 @@ router.put("/:id", multer({storage: storage}).single("image"), (req,res,next) =>
   })
 });
 
-router.delete("/:id", (req, res, next) => {
+router.delete("/:id",checkAuth, (req, res, next) => {
   Post.deleteOne({_id: req.params.id}).then(result => {
     res.status(200).json({message: "Post Deleted"});
   });
