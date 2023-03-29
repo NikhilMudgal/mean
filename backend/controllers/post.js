68c269105650de2896d2392f903def553789c49c
Nikhil Mudgal <nikhilm2097@gmail.com>
2023-03-30 01:24:23 +0530

    fixed update post bug

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -83,7 +83,7 @@ exports.createPosts = (req, res, next) => {
     console.log(post);
     Post.updateOne({_id: req.params.id, creator: req.userData.userId}, post).then(result => {
       console.log(result)
-      if(result.nModified > 0) {
+      if(result.n > 0) {
         res.status(200).json({message: "Update Successfully"});
       } else {
         res.status(401).json({message: "Not Authorised"});
